5c0c5dbc236fa6adaeb1b6dc0f85c6ba4ef429cb
Maccarrone Alessia <maccalessia@gmail.com>
2026-07-17 14:24:38 +0200

    update

--- server-springboot-maps/src/main/java/edu/ing/unict/springboot/server_springboot_maps/messaging/SqsPublisher.java
+++ server-springboot-maps/src/main/java/edu/ing/unict/springboot/server_springboot_maps/messaging/SqsPublisher.java
@@ -6,21 +6,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 @Profile("cloud")
 public class SqsPublisher implements TrafficMessagePublisher {
     private static final Logger logger = LoggerFactory.getLogger(SqsPublisher.class);
-    private final SqsTemplate sqsTemplate;
+    
+    @Autowired(required = false)
+    private SqsTemplate sqsTemplate;
+    
     public static final String QUEUE_NAME = "traffic-data-queue";
 
-    public SqsPublisher(SqsTemplate sqsTemplate) {
-        this.sqsTemplate = sqsTemplate;
+    public SqsPublisher() {
     }
 
     @Override
     public void publishTrafficData(TrafficData trafficData) {
-        logger.info("Publishing to Amazon SQS...");
-        sqsTemplate.send(QUEUE_NAME, trafficData);
+        if (sqsTemplate != null) {
+            logger.info("Publishing to Amazon SQS...");
+            sqsTemplate.send(QUEUE_NAME, trafficData);
+        } else {
+            logger.warn("Amazon SQS is disabled or no credentials found. Skipping message publish.");
+        }
     }
 }
